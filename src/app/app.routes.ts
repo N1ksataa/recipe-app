93cab6b69cb7527a3346e64bcd73b5061d8e5dcc
Nikolay Kastsanev <npkastsanev@gmail.com>
2024-11-29 05:12:51 +0200

    Implement 404 Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
+import { PageNotFoundComponent } from './error/error.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
+
+
+
+
+
+  { path: '404', component: PageNotFoundComponent},
+  { path: '**', redirectTo: '/404'}
 ];
