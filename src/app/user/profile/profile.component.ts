16d338798a3dae8cb1e4428bfd06c3983b60900b
Nikolay Kastsanev <npkastsanev@gmail.com>
2024-12-06 03:09:52 +0200

    Profile Change Details Validator

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,29 +1,63 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { emailValidator } from '../../utils/email.validator';
+import { DOMAINS } from '../../constants';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
 })
-
 export class ProfileComponent {
   showEditInfo = false;
   showChangePassword = false;
 
+  editInfoForm: FormGroup;
+
+  passwordsForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.editInfoForm = this.fb.group({
+      newUsername: ['', [Validators.required, Validators.minLength(6)]],
+      newEmail: ['', [Validators.required, emailValidator(DOMAINS)]],
+    });
+
+    this.passwordsForm = this.fb.group({
+      oldPassword: ['', [Validators.required, Validators.minLength(6)]],
+      newPassword: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
   toggleEditInfo() {
-    if(this.showChangePassword){
+    if (this.showChangePassword) {
       this.showChangePassword = false;
+      this.passwordsForm.reset();
     }
+    this.editInfoForm.reset();
     this.showEditInfo = !this.showEditInfo;
   }
 
   toggleChangePassword() {
-    if(this.showEditInfo){
+    if (this.showEditInfo) {
       this.showEditInfo = false;
+      this.editInfoForm.reset();
     }
+    this.passwordsForm.reset();
     this.showChangePassword = !this.showChangePassword;
+  }
+
+  onSubmitEditInfo() {
+    if (this.editInfoForm.valid) {
+      console.log('Information updated:', this.editInfoForm.value);
+    }
+  }
+
+  onSubmitChangePassword() {
+    if (this.passwordsForm.valid) {
+      console.log('Password changed:', this.passwordsForm.value);
+    }
   }
 }
